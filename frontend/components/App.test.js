59afb31137b17ac7a88ebefa247d40477176501f
Fatima Naz <naz781@gmail.com>
2024-09-16 17:23:47 -0400

    sprint 9

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,23 @@
 // Write your tests here
+import React from 'react';
+import { render, screen, fireEvent} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import AppFunctional from './AppFunctional';
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+test('rendering all coordinates and steps', () => {
+  render(<AppFunctional />);
+  console.log('AppFunctional')
+  expect(screen.getByText("You moved 0 times")).toBeInTheDocument();
+});
+
+test("resets all the state", () => {
+  render(<AppFunctional />);
+  fireEvent.click(screen.getByText("LEFT"));
+  fireEvent.click(screen.getByText("reset"));
+  expect(screen.getByText(/You moved 0 times/)).toBeInTheDocument(); 
+});
+
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 const initialMessage = ''
@@ -7,70 +8,118 @@ const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
+  const [message,setMessage] = useState(initialMessage);
+  const [email,setEmail] = useState(initialEmail);
+  const [steps,setSteps] = useState(initialSteps);
+  const [index,setIndex] = useState(initialIndex);
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
   function getXY() {
+    const x=index % 3 + 1;
+    const y=Math.floor(index / 3) + 1;
+    return {x,y};
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
   }
 
   function getXYMessage() {
+    const {x,y} = getXY();
+    return `Coordinates (${x}, ${y})`;
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
   }
 
   function reset() {
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
+    setIndex(initialIndex);
     // Use this helper to reset all states to their initial values.
   }
 
   function getNextIndex(direction) {
+    if (direction === 'left' && index % 3 !== 0) {
+      return index - 1;//move left
+    } else if (direction === 'right' && index % 3 !== 2) {
+      return index + 1;//mover right
+    } else if (direction === 'up' && index >= 3) {
+      return index - 3;//move up
+    } else if (direction === 'down' && index <= 6) {
+      return index + 3;//move down
+    }
+
+    return index;
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
   }
 
   function move(evt) {
+    const direction = evt.target.id;
+    const nextIndex = getNextIndex(direction);
+    if(nextIndex !== index){
+      setIndex(nextIndex);
+      setSteps(steps+1);
+      setMessage(initialMessage);
+    }else{
+      setMessage(`You can't go ${direction}`);
+    }
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
 
   function onChange(evt) {
-    // You will need this to update the value of the input.
+    const {value} = evt.target;
+    setEmail(value);
   }
 
   function onSubmit(evt) {
+   
+    evt.preventDefault(); 
+    const { x, y } = getXY();
+    const payload = { x, y, steps, email };
+
+    axios.post('http://localhost:9000/api/result', payload)
+      .then(res => {
+        setMessage(res.data.message); 
+        setEmail(initialEmail); 
+      })
+      .catch(err => {
+        setMessage(err.response?.data?.message || 'An error occurred');
+      });
+  
     // Use a POST request to send a payload to the server.
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{getXYMessage()}</h3>
+        <h3 id="steps">You moved {steps} {steps === 1 ? "time" : "times"}</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+              {idx === index ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={move}>LEFT</button>
+        <button id="up" onClick={move}>UP</button>
+        <button id="right" onClick={move}>RIGHT</button>
+        <button id="down" onClick={move}>DOWN</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
